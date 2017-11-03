779667d4470d2363a0aee6b870dd6c5277d74f3f
holma198 <holma198@morris.umn.edu>
2017-11-03 15:31:58 -0500

    Create 2 threads for the client.  One for keyboard input to send to the server, and one to retrieve information from the server.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,28 +1,89 @@
 package echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
+	//private Thread keyThread = null;
+	//private Thread serverThread = null;
+	private boolean closed = false;
+	private static Socket socket;
+	private static InputStream socketInputStream;
+	private static OutputStream socketOutputStream;
+	private static int readByte;
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
+	public static class usrInThread extends Thread {
+		public void run() {
+			try {
+				//System.out.println("AYYYYYyyyeeeee");
+				socket = new Socket("localhost", PORT_NUMBER);
+				socketInputStream = socket.getInputStream();
+				socketOutputStream = socket.getOutputStream();
+				while((readByte = System.in.read()) != -1){
+					socketOutputStream.write(readByte);
+					System.out.println("first While");
+				}
+			}
+			catch(IOException ioE){
+				System.err.println(ioE);
+			}
 		}
-		System.out.flush();
 	}
+
+	public static class serverThread extends Thread {
+		public void run() {
+			try {
+				//System.out.println("234235");
+				int socketByte;
+				while((socketByte = socketInputStream.read()) != -1){
+					System.out.write(socketByte);
+					System.out.println("second While");
+				}
+			} catch(IOException ioE2){
+				System.err.println(ioE2);
+			}
+		}
+	}
+
+	private void start() throws IOException {
+		new usrInThread().start();
+		new serverThread().start();
+
+//		socketOutputStream.close();
+//		socketInputStream.close();
+//		socket.close();
+		System.out.flush();
+
+//		Socket socket = new Socket("localhost", PORT_NUMBER);
+//		InputStream socketInputStream = socket.getInputStream();
+//		InputStream stdIn = System.in;
+//		BufferedReader in = new BufferedReader(new InputStreamReader(stdIn));
+//
+//		OutputStream socketOutputStream = socket.getOutputStream();
+//		int readByte;
+//
+//		//keyThread.start();
+//		while ((readByte = System.in.read()) != -1) {
+//			socketOutputStream.write(readByte);
+//		}
+//		int readByte;
+//		while ((readByte = System.in.read()) != -1) {
+//			socketOutputStream.write(readByte);
+//			int socketByte = socketInputStream.read();
+//			System.out.write(socketByte);
+//		}
+	}
+//	private void run() throws IOException {
+//
+//		int socketByte = socketInputStream.read();
+////			System.out.write(socketByte);
+//
+//		System.out.flush();
+//	}
 }
